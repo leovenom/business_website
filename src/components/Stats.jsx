ed1cd622aa38dc52406850bd25acf8c6b425592f
leovenom <leonardt@outlook.com>
2024-04-05 17:24:46 +0100

    business

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,8 +3,8 @@ import styles from'../style';
 
 const Stats = () => (
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
-      {stats.map((stat) => (
-        <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row mr-3`}>
+      {stats.map((stat, index) => (
+        <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row ${index !== stats.length - 1 ? 'mr-3': 'mr-0'}`}>
           <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">{stat.value}</h4>
           <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">{stat.title}</p>
         </div>
